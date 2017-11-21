80b79cd4684af41a8b4cc44373f72ef90a774397
顾尧 <gamesgao@gmail.com>
2017-11-21 13:22:47 +0800

    Implemented the service property API and supporting converting function.

--- azure/storage/constants.rb
+++ azure/storage/constants.rb
@@ -33,6 +33,7 @@ module Azure
         TICKS_PER_MILLISECOND = 10000
         UNIX_EPOCH_IN_MILLISECONDS = 62135596800000
         MILLISECONDS_PER_SECOND = 1000
+        HTTP_Methods = ["DELETE", "GET", "HEAD", "MERGE", "POST", "OPTIONS", "PUT"]
       end
     end
   end
